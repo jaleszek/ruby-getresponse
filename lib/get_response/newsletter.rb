0a3d31360efdee78dc008c5b170003445d7b6a34
jaleszek <andrukanisleszek@gmail.com>
2012-10-02 18:12:33 +0300

    sending newsletter feature

--- lib/get_response/newsletter.rb
+++ lib/get_response/newsletter.rb
@@ -1,7 +1,24 @@
 module GetResponse
 
   class Newsletter < Message
+    
+    def initialize(params, connection)
 
+      @campaign_id = params["campaign_id"]
+      @from_field = params["from_field"]
+      @reply_to_field = params["reply_to_field"]
+      @contacts = params["contacts"]
+
+      super(params, connection)
+    end
+    
+    #TODO returned value + units
+    def send(contacts)
+      params = {"campaign" => @campaign_id, "from_field" => @from_field, "reply_to_field" => @reply_to_field, "subject" => @subject, "contents" => {"plain" => @text_content, "html" => @html_content }, "contacts" => contacts}
+      resp = @connection.send_request("send_newsletter", params)
+      
+    end
+    
     # Delete newsletter. You can delete only newsletters that have send_on date in future. If you try
     # to delete exception will be raised.
     def destroy
